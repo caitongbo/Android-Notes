95ba893bd752b83b2966b86a22cf3639725e6ffa
caitongbo <461892420@qq.com>
2018-06-07 13:59:17 +0800

    update

---
+++ LoginLayout/app/src/main/java/com/example/samsung/loginlayout/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.samsung.loginlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(Main2Activity.this,"登录成功！",Toast.LENGTH_SHORT).show();
+    }
+}
